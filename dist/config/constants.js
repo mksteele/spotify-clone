f08d27757e7e0e8a9bf0a40995553f947393a69c
Miranda Steele <steelmir@gmail.com>
2020-07-21 18:15:35 -0700

    This kind of works.
    Run
    $ yarn build
    $ yarn start

---
+++ dist/config/constants.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.PORT = void 0;
+// Default to port 4000 unless .env file specifies otherwise
+exports.PORT = process.env.PORT || 4000;
+//# sourceMappingURL=constants.js.map
