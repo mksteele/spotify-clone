cc2fb9f03f60c997d1a10422bc8aad6913e7e109
Miranda Steele <steelmir@gmail.com>
2020-07-21 17:55:15 -0700

    Moving around

--- old/src/api/abstract-controller.ts
+++ old/src/api/abstract-controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 
-export abstract class AbstractCrudObject {
+export abstract class AbstractController {
   public abstract create(req: Request, res: Response): void;
   public abstract read(req: Request, res: Response): void;
   public abstract update(req: Request, res: Response): void;
--- old/src/api/songs.ts
+++ old/src/api/songs.ts
@@ -1,4 +1,43 @@
 import { Request, Response } from "express";
-import { AbstractCrudObject } from "./abstract-crud-object";
+import { AbstractController } from "./abstract-controller";
 
-export class SongController extends AbstractCrudObject {}
+import { Get, Route, Tags } from "tsoa";
+// TODO
+// import { ServerError } from "../utils/server-error";
+
+@Route("songs")
+export class SongController extends AbstractController {
+
+}
+
+export interface ISong {
+  id: number;
+  label: string;
+  color: string;
+}
+
+const widgets: IWidget[] = [
+  {
+    color: "blue",
+    id: 1,
+    label: "first widget",
+  },
+];
+
+  @Get()
+  @Tags("Widgets")
+  public async listSongs(): Promise<IWidget[]> {
+    return widgets;
+  }
+
+  @Get("{widgetId}")
+  @Tags("Widgets")
+  public async GetWidget(widgetId: number): Promise<IWidget> {
+    const widget = widgets.find((w) => w.id === widgetId);
+    if (!widget) {
+      throw new ServerError(`no widget found with id ${widgetId}`);
+    }
+
+    return widget;
+  }
+}
